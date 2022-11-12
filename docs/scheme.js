f8d28f39b1b21a935e2d0943590379f9681e2127
Vincent Mills <vincentmills273@gmail.com>
2022-11-11 22:31:52 -0800

    added a way for photos to pop up

--- docs/scheme.js
+++ docs/scheme.js
@@ -1,31 +1,21 @@
-$(document).ready(function() {
-    $("html").addClass('loaded');
-    if (localStorage.getItem('dark') === null) {
-        localStorage.setItem('dark', true);
-    }
+$(document).ready(function () {
+	$("html").addClass("loaded");
+	if (localStorage.getItem("dark") === null) {
+		localStorage.setItem("dark", true);
+	}
 
-    let darkDefault = localStorage.getItem('dark') === 'true';
+	let darkDefault = localStorage.getItem("dark") === "true";
 
+	function setMode(lightMode) {
+		$(".scheme div").text(lightMode ? "light_mode" : "dark_mode");
+		$(":root").toggleClass("light-mode", lightMode);
+	}
 
-    function setMode(lightMode) {
-        $(".scheme div").text(lightMode ? "light_mode" : "dark_mode");
-        $(":root").toggleClass("light-mode", lightMode);
-    }
+	setMode(!darkDefault);
 
-    setMode(!darkDefault);
-
-    $(".scheme").click(function() {
-        darkDefault = !darkDefault;
-        setMode(!darkDefault);
-        localStorage.setItem('dark', darkDefault)
-    })
-/*
-    // TODO: delete this.
-    $(".follow").click(function() {
-        $(":root").toggleClass('side-flip');
-    })
-
-    $(".credit").click(function() {
-        $(":root").toggleClass('post-flip');
-    })*/
+	$(".scheme").click(function () {
+		darkDefault = !darkDefault;
+		setMode(!darkDefault);
+		localStorage.setItem("dark", darkDefault);
+	});
 });
